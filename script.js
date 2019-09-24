9f84fd961e2a6daa34574d655b9aa320b745ba8b
Paul Saada <paulsaada6@gmail.com>
2019-09-24 11:18:55 -0400

    optimized tween speeds

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-TweenMax.from(".title", 0.8, { opacity: 0, ease: Power0.easeIn });
-TweenMax.from(".intro-desc", 0.8, { opacity: 0, delay: 0.5, ease: Power0.easeIn });
-TweenMax.from(".see-work", 0.8, { opacity: 0, delay: 2.1, ease: Power0.easeIn })
-TweenMax.from(".illustration", 0.8, { opacity: 0, delay: 1, ease: Power0.easeIn });
-
+TweenMax.from(".title", 0.8, { opacity: 0, x: -100, ease: Power0.easeIn });
+TweenMax.from(".intro-desc", 0.8, { opacity: 0, x: -100, delay: 0.2, ease: Power1.easeIn });
+TweenMax.from(".see-work", 0.8, { opacity: 0, delay: 1.3, ease: Power0.easeIn })
+TweenMax.from(".illustration", 0.5, { opacity: 0, y: 100, delay: 0.3, ease: Power0.easeIn });
+TweenMax.from(".nav", 0.5, { opacity: 0, y: -100, ease: Power0.easeIn });
