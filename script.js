46e95c8ed0763bc30db02fc746ef5eef2edf5c8b
Paul Saada <paulsaada6@gmail.com>
2019-09-24 09:56:16 -0400

    completed gsap animations on homepage

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-TweenMax.staggerFrom(".title", 0.5, { opacity: 0, y: "200" }, 0.2);
+TweenMax.from(".title", 0.8, { opacity: 0, ease: Power0.easeIn });
+TweenMax.from(".intro-desc", 0.8, { opacity: 0, delay: 1, ease: Power0.easeIn });
+TweenMax.from(".see-work", 0.8, { opacity: 0, delay: 3, ease: Power0.easeIn })
+TweenMax.from(".illustration", 0.8, { opacity: 0, delay: 2, ease: Power0.easeIn });
+
