a645c28285b8a1baed6b9fe17f1548042ad4517e
Paul Saada <paulsaada6@gmail.com>
2019-09-24 10:48:25 -0400

    shortened CTA animation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 TweenMax.from(".title", 0.8, { opacity: 0, ease: Power0.easeIn });
 TweenMax.from(".intro-desc", 0.8, { opacity: 0, delay: 0.5, ease: Power0.easeIn });
-TweenMax.from(".see-work", 0.8, { opacity: 0, delay: 2.5, ease: Power0.easeIn })
+TweenMax.from(".see-work", 0.8, { opacity: 0, delay: 2.1, ease: Power0.easeIn })
 TweenMax.from(".illustration", 0.8, { opacity: 0, delay: 1, ease: Power0.easeIn });
 
