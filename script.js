d7d92ffbf03900da6d16226c330fd4cad0bfac86
Paul Saada <paulsaada6@gmail.com>
2019-09-25 08:01:50 -0400

    added button styles, moved download resume button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // homepage
-TweenMax.from(".title", 0.8, { opacity: 0, x: -100, ease: Power0.easeIn });
+TweenMax.from(".title", 1.7, { opacity: 0, y: -100, ease: Circ.easeOut });
 TweenMax.from(".intro-desc", 0.8, { opacity: 0, x: -100, delay: 0.2, ease: Power1.easeIn });
 TweenMax.from(".see-work", 0.8, { opacity: 0, delay: 1.3, ease: Power0.easeIn })
 TweenMax.from(".illustration", 0.5, { opacity: 0, y: 100, delay: 0.3, ease: Power0.easeIn });
